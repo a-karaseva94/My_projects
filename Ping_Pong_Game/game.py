aae8c1f5fd81d859e9422e88b0bd4b20e4a30ca0
a-karaseva94 <a-karaseva94@mail.ru>
2024-12-23 13:12:20 +0300

    Add docstrings

--- Ping_Pong_Game/game.py
+++ Ping_Pong_Game/game.py
@@ -1,11 +1,16 @@
 import arcade
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = 'Pong game'
+SCREEN_WIDTH = 800 # ширина окна
+SCREEN_HEIGHT = 600 # высота окна
+SCREEN_TITLE = 'Pong game' # заголовок окна
 
 
 class Ball(arcade.Sprite):
+    """
+    Класс для мяча:
+    - в init подгружаем изображение и устанавливаем размер;
+    - функция update отвечает за логику движения (в т.ч. ограничение по границам окна).
+    """
     def __init__(self):
         super().__init__('ball.png', 0.04)
         self.change_x = 4
@@ -25,6 +30,11 @@ class Ball(arcade.Sprite):
 
 
 class Bar(arcade.Sprite):
+    """
+    Класс для ракетки:
+    - в init подгружаем изображение и устанавливаем размер;
+    - функция update отвечает за логику движения (в т.ч. ограничение по границам окна).
+    """
     def __init__(self):
         super().__init__('bar.png', 0.35)
         self.change_x = 3
@@ -42,36 +52,54 @@ class Game(arcade.Window):
         super().__init__(width, height, title)
         self.ball = Ball()
         self.bar = Bar()
-        self.setup()
+        self.setup() # не метод Windows, поэтому вызываем
 
     def setup(self):
+        """
+        Положение элементов в игре.
+        """
         self.bar.center_x = SCREEN_WIDTH / 2
         self.bar.center_y = SCREEN_HEIGHT / 5
         self.ball.center_x = SCREEN_WIDTH / 2
         self.ball.center_y = SCREEN_HEIGHT / 2
 
     def on_draw(self):
+        """
+        Отрисовка элементов на окне игры:
+        clear - очистка окна (закрашивание окна белым);
+        draw отрисовывает ракетку и мячик.
+        """
         self.clear((255, 255, 255))
         self.ball.draw()
         self.bar.draw()
 
     def update(self, delta):
+        """
+        Обновление информации о текущем состоянии элемента (запуск методов Sprites)
+        arcade.check_for_collision проверяет Sprites на столкновение и отталкивает мяч
+        """
         if arcade.check_for_collision(self.bar, self.ball):
             self.ball.change_y = -self.ball.change_y
         self.ball.update()
         self.bar.update()
 
     def on_key_press(self, key, modifiers):
+        """
+        Управление ракеткой с помощью клавиатуры (нажатие кнопки) - движение.
+        """
         if key == arcade.key.RIGHT:
             self.bar.change_x = 5
         if key == arcade.key.LEFT:
             self.bar.change_x = -5
 
     def on_key_release(self, key, modifiers):
+        """
+        Управление ракеткой с помощью клавиатуры (отпускание кнопки) - остановка
+        """
         if key == arcade.key.RIGHT or key == arcade.key.LEFT:
             self.bar.change_x = 0
 
 
 if __name__ == '__main__':
-    window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    arcade.run()
+    window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)  # окно игры
+    arcade.run()  # цикл обновления
